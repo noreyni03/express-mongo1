4f9ac216997ced5b920705d7adabe750902f86ab
ElBachir Toure <bassirou.toure@esp.sn>
2024-04-23 11:45:57 +0000

    delete and deleteAll

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -46,3 +46,41 @@ exports.findAll = (req, res) => {
       });
     });
 };
+
+// Delete a Student with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  Student.findByIdAndRemove(id)
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Student with id=${id}. Maybe Student was not found!`,
+        });
+      } else {
+        res.send({
+          message: "Student was deleted successfully!",
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Student with id=" + id,
+      });
+    });
+};
+
+// Delete all Students from the database.
+exports.deleteAll = (req, res) => {
+  Student.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Students were deleted successfully!`,
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all students.",
+      });
+    });
+};
--- routes/student.routes.js
+++ routes/student.routes.js
@@ -8,5 +8,11 @@ module.exports = app => {
   // Retrieve all Students
   router.get("/", students.findAll);
 
+  // Delete a Student with id
+  router.delete("/:id", students.delete);
+
+  // Delete all students
+  router.delete("/", students.deleteAll);
+
   app.use("/api/students", router);
 };
